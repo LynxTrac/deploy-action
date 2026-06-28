8bd349d623159d8d72b2a39b44d66ea9765388b6
Justin Samuelraj <justin@beesics.com>
2026-06-29 01:22:30 +0530

    Sample test from localhost for dev purpose

---
+++ test.js
@@ -0,0 +1,8 @@
+process.env.INPUT_APIKEY = '1q2w3e4r5t';
+process.env.INPUT_BLUEPRINT = 'test-blueprint';
+process.env.INPUT_VERSION = '1.2.3';
+process.env.INPUT_DEPLOYMENT_REQUEST = JSON.stringify({
+  deploymentModel: 'LRT12'
+});
+
+require('./dist/index');
